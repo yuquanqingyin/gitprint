ca63bc858bcfa52e842ef2b5ce3418f97d10083d
Alex Pliutau <a.pliutau@gmail.com>
2024-10-11 20:01:45 +0200

    fix header

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -32,17 +32,19 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className="flex flex-col h-screen justify-between">
         <header className="flex h-16 w-full items-center justify-center border-b">
-          <span className="mx-2">
-            <a href="/">home</a>
-          </span>
-          <span>•</span>
-          <span className="mx-2">
-            <a href="/docs">docs</a>
-          </span>
-          <span>•</span>
-          <span className="mx-2">
-            <a href="/contact">contact</a>
-          </span>
+          <div className="flex justify-center p-4 text-sm">
+            <span className="mx-2">
+              <a href="/">home</a>
+            </span>
+            <span>•</span>
+            <span className="mx-2">
+              <a href="/docs">docs</a>
+            </span>
+            <span>•</span>
+            <span className="mx-2">
+              <a href="/contact">contact</a>
+            </span>
+          </div>
         </header>
         <main className="flex flex-col mb-auto">{children}</main>
         <footer className="flex w-full items-center justify-center border-t">
